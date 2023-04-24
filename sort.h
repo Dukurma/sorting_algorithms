17d827d3837c23a67ed27587477ec3a14ed3fcd3
Dukurma <barryyabilsu@gmail.com>
2023-04-24 01:54:49 -0700

    zero

--- sort.h
+++ sort.h
@@ -24,13 +24,13 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* SORT ALGORITHMS */
+/* SORTED ALGORITHMS */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-/* ADVANCED SORT ALGORITHM */
+/* ADVANCED SORTED ALGORITHM */
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
